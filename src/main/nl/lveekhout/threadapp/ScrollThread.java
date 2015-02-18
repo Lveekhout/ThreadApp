4e84a7473464ed4412b6302003dafa4eabed86de
Lveekhout <leekhout@hotmail.com>
2015-02-18 17:48:43 +0100

    Het begin is er...

---
+++ src/main/nl/lveekhout/threadapp/ScrollThread.java
@@ -0,0 +1,12 @@
+package nl.lveekhout.threadapp;
+
+/**
+ * Created by leekhout on 18-2-2015.
+ * class ScrollThread
+ */
+
+public class ScrollThread {
+    public static void main(String args[]) {
+        System.out.println("Dit is een test");
+    }
+}
